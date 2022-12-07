5327ced601d580eb46a000fe9e2cad37c35083f7
Andrés Pereira de Lucena <andreslucena@gmail.com>
2022-12-07 13:19:44 +0100

    Solve "uninitialized constant Spring::Watcher::Listen" exception
    This was when trying to open the rails console with
    
    bin/rails console -e staging
    
    We should report this and fix it upstream.

--- config/spring.rb
+++ config/spring.rb
@@ -1,8 +1,10 @@
-require "decidim/spring"
+if Object.const_defined?("Spring::Watcher::Listen")
+  require "decidim/spring"
 
-Spring.watch(
-  ".ruby-version",
-  ".rbenv-vars",
-  "tmp/restart.txt",
-  "tmp/caching-dev.txt"
-)
+  Spring.watch(
+    ".ruby-version",
+    ".rbenv-vars",
+    "tmp/restart.txt",
+    "tmp/caching-dev.txt"
+  )
+end
